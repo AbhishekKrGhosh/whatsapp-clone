a50d3f7da3fe484964760bf44749545efe78670e
Abhishek Kumar <abhishek.andri05@gmail.com>
2023-12-07 23:16:05 +0530

    login page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import Messenger from "./components/Messenger";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
+  const clientId = '933451419685-joq9oulsciicc98nc0sk0bsuerbma4hf.apps.googleusercontent.com'
   return (
-    <div>
+    <GoogleOAuthProvider clientId={clientId}>
      <Messenger/>
-    </div>
+    </GoogleOAuthProvider>
   );
 }
 
--- client/src/components/Messenger.js
+++ client/src/components/Messenger.js
@@ -1,10 +1,27 @@
 import React from "react";
+import {AppBar, Toolbar, styled, Box} from '@mui/material'
+import LoginDialog from "./account/LoginDialog";
+
+const Header = styled(AppBar)`
+    height:220px;
+    background-color: #00a884;
+    box-shadow: none;
+`
+const Component = styled(Box)`
+    height:100vh;
+    background:#f0f2f5;
+`
 
 function Messenger(){
     return(
-        <div>
-            Messenger
-        </div>
+        <Component>
+        <Header>
+            <Toolbar>
+
+            </Toolbar>
+        </Header>
+        <LoginDialog/>
+        </Component>
 )
 }
 
